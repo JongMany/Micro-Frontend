237bfc519d16feb20aeb0b54ddc74ab4e01d2ddc
JongMany <blackberry1114@naver.com>
2025-01-11 23:34:51 +0900

    products moduleFederationPlugin 추가

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 module.exports = {
   mode: 'development',
   // Dev Server 열기
@@ -9,6 +11,17 @@ module.exports = {
     // 빌드 결과물을 HTML에 추가(연결)
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    // Module Federation Plugin 추가
+    new ModuleFederationPlugin({
+      // 이름 지정
+      name: 'products',
+      // 파일 이름 지정
+      filename: 'remoteEntry.js',
+      // export할 모듈 지정
+      exposes: {
+        './ProductsIndex': './src/index',
+      },
     }),
   ],
 };
